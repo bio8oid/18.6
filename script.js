7faaf1932bece0bdc7ba4a2a3a2dea2fee86f514
bio8oid <8bollod8@mail.co.uk>
2019-06-26 01:33:23 +0200

    Update script.file

--- script.js
+++ script.js
@@ -5,18 +5,6 @@ var Counter = React.createClass({
         };
     },
 
-    getDefaultProps: function() {
-       //console.log(getDefaultProps);
-    },
-
-    componentWillMount: function() {
-       //console.log(componentWillMount);
-    },
-
-    componentDidMount: function() {
-       //console.log(componentDidMount);
-    }, 
-
     increment: function() {
         this.setState({
             counter: this.state.counter + 1
@@ -33,84 +21,18 @@ var Counter = React.createClass({
 
     render: function() {
         return React.createElement('div', {},
-            React.createElement('span', {onClick: this.increment}, ' plus ' + this.state.counter),
-
-             React.createElement('span', {onClick: this.decrement}, ' minus '),
-
-
-            React.createElement('span', {onClick: this.increment}, ' plus ' + this.state.counter),
-
-             React.createElement('span', {onClick: this.decrement}, ' minus ')
-
+            React.createElement('button', {onClick: this.increment}, ' plus ' + this.state.counter),
+            React.createElement('button', {onClick: this.decrement}, ' minus ')
         );
-       
-    }
-});
-
-var deCounter = React.createClass({
-    getInitialState: function() {
-        return {
-            counter: 0
-        };
-    },
-
-    getDefaultProps: function() {
-       //console.log(getDefaultProps);
-    },
-
-    componentWillMount: function() {
-       //console.log(componentWillMount);
-    },
-
-    componentDidMount: function() {
-       //console.log(componentDidMount);
-    },
-
-    increment: function() {
-        this.setState({
-            counter: this.state.counter + 1
-        });
-            console.log();
-    },
-
-    decrement: function() {
-        this.setState({
-            counter: this.state.counter - 1
-        });
-    },
-
-
-    render: function() {
-        return React.createElement('div', {},
-            React.createElement('span', {onClick: this.increment}, ' plus ' + this.state.counter),
-
-             React.createElement('span', {onClick: this.decrement}, ' minus '),
-
-
-            React.createElement('span', {onClick: this.increment}, ' plus ' + this.state.counter),
-
-             React.createElement('span', {onClick: this.decrement}, ' minus ')
-
-        );
-       
     }
 });
 
 
 
-//var element = React.createElement(Counter);
-//ReactDOM.render(element, document.getElementById('app'));
+var element = React.createElement('div', {},
+    React.createElement(Counter),
+    React.createElement(Counter),
+    React.createElement(Counter)
+)
 
-
-var List = React.createClass({
-
-  render: function() {
-    return (
-        React.createElement(Counter),
-        React.createElement(deCounter)
-    )
-  },
-});
-
-
-ReactDOM.render(React.createElement(List), document.getElementById('app'));
+ReactDOM.render(element, document.getElementById('app'));
